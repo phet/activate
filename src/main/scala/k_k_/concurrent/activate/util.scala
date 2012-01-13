10a1991c232dfde2da49fcdff7ef5eccd0ad4ccf
Kip <tie.illustrates@gmail.com>
2012-01-13 09:18:15 -0800

    Update copyright to 2012.

--- src/main/scala/k_k_/concurrent/activate/util.scala
+++ src/main/scala/k_k_/concurrent/activate/util.scala
@@ -2,7 +2,7 @@
  * Activate Library for Scala - Concurrency-Oriented Programming in the
  * Activity Activation Style.
  *
- * Copyright (c)2009-2011 by Corbin "Kip" Kohn
+ * Copyright (c)2009-2012 by Corbin "Kip" Kohn
  * All Rights Reserved.
  *
  *  Released under the terms of the Artistic License 2.0; see:
